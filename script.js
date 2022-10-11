86358cfacf51bad4d53e50b28f0ead8384f49963
Bruno Morgades <brunomorgades@hotmail.com>
2022-10-10 23:29:14 -0300

    baloons working have to be fixed

--- script.js
+++ script.js
@@ -1,34 +1,35 @@
 let index=0;
 let calc=(-1 * ((720 *2) + 95 ));
-let project=document.querySelectorAll(".projects__project")[0];
-// for(i=0;i<5;i++){
-//     project.push()
-// }
+let project=[];
+let circle=[];
+let size=[];
+for(i=0;i<5;i++){
+    project.push(document.querySelectorAll(".projects__project")[i]);
+    circle.push(document.querySelectorAll(".projects__circle")[i]);
+    i>2?
+    size.push(`projects__circle--2`):
+    size.push(`projects__circle--${i}`);
+}
+size.push(`projects__circle--1`);
+console.log(size);
+// size.reverse();
 const prev=()=>{
     index>0?index<5?index--:index=4:index=0;
-    document.querySelectorAll(".projects__project")[0].style.transform=`translateX(${index*calc}px)`;
-    document.querySelectorAll(".projects__project")[1].style.transform=`translateX(${index*calc}px)`;
-    document.querySelectorAll(".projects__project")[2].style.transform=`translateX(${index*calc}px)`;
-    document.querySelectorAll(".projects__project")[3].style.transform=`translateX(${index*calc}px)`;
-    document.querySelectorAll(".projects__project")[4].style.transform=`translateX(${index*calc}px)`;
-    // document.querySelectorAll(".projects__circle")[0].classList.remove(`projects__circle--${index+2}`);
-    // document.querySelectorAll(".projects__circle")[0].classList.add(`projects__circle--${index+1}`);
+    project.forEach(item=>item.style.transform=`translateX(${index*calc}px)`);
+    circle.forEach((item,i)=>item.classList.remove(size[i]));
+    let first=size.shift();
+    size.push(first);
+    circle.forEach((item,i)=>item.classList.add(size[i]));
     console.log(index)
 }
 const next=()=>{
+    console.log("index",index);
     index>=0?index<4?index++:index=4:index=0;
-    document.querySelectorAll(".projects__project")[0].style.transform=`translateX(${index*calc}px)`;
-    document.querySelectorAll(".projects__project")[1].style.transform=`translateX(${index*calc}px)`;
-    document.querySelectorAll(".projects__project")[2].style.transform=`translateX(${index*calc}px)`;
-    document.querySelectorAll(".projects__project")[3].style.transform=`translateX(${index*calc}px)`;
-    document.querySelectorAll(".projects__project")[4].style.transform=`translateX(${index*calc}px)`;
-    // if(index!=0){
-    // document.querySelectorAll(".projects__circle")[0].classList.remove(`projects__circle--${index}`);
-    // document.querySelectorAll(".projects__circle")[0].classList.add(`projects__circle--${index+1}`);
-    // document.querySelectorAll(".projects__circle")[1].classList.remove(`projects__circle--${index+1}`);
-    // document.querySelectorAll(".projects__circle")[1].classList.add(`projects__circle--${index>5?index-3:index}`);
-    // }
-    console.log(index)
+    project.forEach(item=>item.style.transform=`translateX(${index*calc}px)`);
+    circle.forEach((item,i)=>item.classList.remove(size[i]));
+    let first=size.pop();
+    size.unshift(first);
+    circle.forEach((item,i)=>item.classList.add(size[i]));
 }
 
 
