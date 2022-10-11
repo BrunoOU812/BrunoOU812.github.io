b9d563b3cc7ece10811e3dbe6308234f727c973b
Bruno Morgades <brunomorgades@hotmail.com>
2022-10-11 20:37:29 -0300

    adding header and some ux functionalities

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ let calc=(-1 * ((720 *2) + 95 ));
 let project=[];
 let circle=[];
 let size=[];
+let scrolled=false;
 for(i=0;i<5;i++){
     project.push(document.querySelectorAll(".projects__project")[i]);
     circle.push(document.querySelectorAll(".projects__circle")[i]);
@@ -11,8 +12,6 @@ for(i=0;i<5;i++){
     size.push(`projects__circle--${i}`);
 }
 size.push(`projects__circle--1`);
-console.log(size);
-// size.reverse();
 const prev=()=>{
     index>0?index<5?index--:index=4:index=0;
     project.forEach(item=>item.style.transform=`translateX(${index*calc}px)`);
@@ -31,7 +30,22 @@ const next=()=>{
     size.unshift(first);
     circle.forEach((item,i)=>item.classList.add(size[i]));
 } 
-// s
 
 document.querySelector(".projects__widget--left").addEventListener("click",()=>index>0?prev():"");
 document.querySelector(".projects__widget--right").addEventListener("click",()=>index<4?next():"");
+
+
+document.addEventListener('scroll', (e) => {
+    if(window.scrollY>500){
+        document.querySelector(".nav").style.backgroundColor = "rgba( 0,0,0,0.3)";
+    }
+    else{
+        document.querySelector(".nav").style.backgroundColor = "rgba( 0,0,0,1)";
+    };
+  });
+
+  document.addEventListener('scroll', () => {
+        index<4?window.scrollY>=870?scrolled==false?next():"":"":"";
+        window.scrollY>=870?scrolled=true:""
+        console.log(scrolled);
+    })
